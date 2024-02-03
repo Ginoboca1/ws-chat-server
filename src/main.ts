817e73884dee79480f82f79d7aca6d3627308947
Gino Bocacorsicopicolino <gnoboca@gmail.com>
2024-02-03 09:54:10 -0300

    allow all origins

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://ws-chat-client-theta.vercel.app',
-    ],
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true,
   });
